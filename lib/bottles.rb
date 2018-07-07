d1935db929d2e28dfeaa0606ebebb408b8eafe79
Ssamael <hikokoro06@gmail.com>
2018-07-07 10:30:20 +0200

    Made first test green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,6 @@
 class Bottles
+  def bla
+    '99 bottles of beer on the wall, 99 bottles of beer./nTake one down and pass it around, 98 bottles of beer on the wall.'
+  end
 
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
+    subject {Bottles.new}
 
+    it 'returns 99 bottles' do 
+      expect(subject.bla).to eq '99 bottles of beer on the wall, 99 bottles of beer./nTake one down and pass it around, 98 bottles of beer on the wall.'
+      end        
+    
 end
