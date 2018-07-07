62e147e7a0dfc8f868dee72892ffb3a80a0cbc15
Ssamael <hikokoro06@gmail.com>
2018-07-07 11:36:20 +0200

    Made 6th test pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,8 @@
 class Bottles
   def verse(number)
     if number > 2 && number <= 99
-        "#{number} bottles of beer on the wall, #{number} bottles of beer./nTake one down and pass it around, #{number - 1} bottles of beer on the wall."
+        "#{number} bottles of beer on the wall, #{number} bottles of beer."\
+        "/nTake one down and pass it around, #{number - 1} bottles of beer on the wall."
     elsif number == 2
         '2 bottles of beer on the wall, 2 bottles of beer.'\
         '/nTake one down and pass it around, 1 bottle of beer on the wall.'
@@ -13,5 +14,36 @@ class Bottles
         '/nGo to the store and buy some more, 99 bottles of beer on the wall.'
     end
   end
+  
+  def verses(number1, number2)
 
+    if number1 > 2 && number1 <= 99
+        string1 = "#{number1} bottles of beer on the wall, #{number1} bottles of beer."\
+        "/nTake one down and pass it around, #{number1 - 1} bottles of beer on the wall."
+    elsif number1 == 2
+        string1 = '2 bottles of beer on the wall, 2 bottles of beer.'\
+        '/nTake one down and pass it around, 1 bottle of beer on the wall.'
+    elsif number1 == 1
+        string1 = '1 bottle of beer on the wall, 1 bottle of beer.'\
+        '/nTake it down and pass it around, no more bottles of beer on the wall.'
+    else
+        string1 = 'No more bottles of beer on the wall, no more bottles of beer.'\
+        '/nGo to the store and buy some more, 99 bottles of beer on the wall.'
+    end
+    if number2 > 2 && number2 <= 99
+        string2 = "#{number2} bottles of beer on the wall, #{number2} bottles of beer."\
+        "/nTake one down and pass it around, #{number2 - 1} bottles of beer on the wall."
+    elsif number2 == 2
+        string2 = '2 bottles of beer on the wall, 2 bottles of beer.'\
+        '/nTake one down and pass it around, 1 bottle of beer on the wall.'
+    elsif number2 == 1
+        string2 = '1 bottle of beer on the wall, 1 bottle of beer.'\
+        '/nTake it down and pass it around, no more bottles of beer on the wall.'
+    else
+        string2 = 'No more bottles of beer on the wall, no more bottles of beer.'\
+        '/nGo to the store and buy some more, 99 bottles of beer on the wall.'
+    end
+    
+    string1 + '/n' + string2 
+  end
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -24,5 +24,13 @@ RSpec.describe Bottles do
     it '0 bottles' do
         expect(subject.verse(0)).to eq 'No more bottles of beer on the wall, no more bottles of beer./nGo to the store and buy some more, 99 bottles of beer on the wall.'
     end
+
+    it '99 and 98 bottles' do
+        expect(subject.verses(99,98)).to eq '99 bottles of beer on the wall, 99 bottles of beer.'\
+        '/nTake one down and pass it around, 98 bottles of beer on the wall.'\
+        '/n98 bottles of beer on the wall, 98 bottles of beer.'\
+        '/nTake one down and pass it around, 97 bottles of beer on the wall.'
+       
+    end
   end
 end
