c32849fb46671bc0ab8e3443a8d8ac0ca982bb4a
Ssamael <hikokoro06@gmail.com>
2018-07-07 10:59:50 +0200

    Made third test (for 2 bottles) pass and little refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,7 +3,10 @@ class Bottles
     if number == 99
         '99 bottles of beer on the wall, 99 bottles of beer./nTake one down and pass it around, 98 bottles of beer on the wall.'
     elsif number == 89
-    '89 bottles of beer on the wall, 89 bottles of beer./nTake one down and pass it around, 88 bottles of beer on the wall.'
+        '89 bottles of beer on the wall, 89 bottles of beer./nTake one down and pass it around, 88 bottles of beer on the wall.'
+    elsif number == 2
+        '2 bottles of beer on the wall, 2 bottles of beer./nTake one down and pass it around, 1 bottle of beer on the wall.'
+
     end
   end
 
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -3,13 +3,18 @@ require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
     subject {Bottles.new}
+  describe 'Return proper message i case of' do
 
-    it 'returns 99 bottles' do 
+    it '99 bottles' do 
       expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer./nTake one down and pass it around, 98 bottles of beer on the wall.'
       end        
 
-    it 'in case of 89 bottles' do
+    it '89 bottles' do
         expect(subject.verse(89)).to eq '89 bottles of beer on the wall, 89 bottles of beer./nTake one down and pass it around, 88 bottles of beer on the wall.'
     end
-    
+
+    it '2 bottles' do
+        expect(subject.verse(2)).to eq '2 bottles of beer on the wall, 2 bottles of beer./nTake one down and pass it around, 1 bottle of beer on the wall.'
+    end
+  end
 end
